3f993fa501df9bb068b068932c926f55b7e42e74
avik94ab <avik94ab@gmail.com>
2022-11-02 10:34:55 -0500

    readPDBfile() atom list generated- check area calc

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ W_Sconf=1.0
 Current_Temp = 298.15
 ASA_exposed_amide=7.5
 
+'''
+Radius of atoms table
+'''
+radius_table = {"N": 1.650, "CA": 1.870, "C": 1.760,
+                "O": 1.400, "AD1": 1.700, "AD2": 1.700,
+                "AE1": 1.700, "AE2": 1.700, "CB": 1.870,
+                "CD": 1.870, "CD1": 1.760, "CD2": 1.760,
+                "CG": 1.870, "CG1": 1.870, "CG2": 1.870,
+                "CE": 1.870, "CE1": 1.760, "CE2": 1.760,
+                "CE3": 1.760, "CH2": 1.760, "CZ": 1.870,
+                "CZ2": 1.760, "CZ3": 1.760, "ND": 1.650,
+                "ND1": 1.650, "ND2": 1.650, "NE": 1.650,
+                "NE1": 1.650, "NE2": 1.650, "NH1": 1.650,
+                "NH2": 1.650, "NZ": 1.650, "OD": 1.400,
+                "OD1": 1.400, "OD2": 1.400, "OT2": 1.400,
+                "OE": 1.400, "OE1": 1.400, "OE2": 1.400,
+                "OH2": 1.400, "OG": 1.400, "OG1": 1.400,
+                "OT1": 1.400, "OXT": 1.400, "OH": 1.400,
+                "SD": 1.850, "SG": 1.850, "P": 1.900
+                }
+
+
 
 def partition_generator(seq_length, window_size, Minimum_Window_Size):
     startIndex = list(range(window_size))
@@ -69,13 +91,15 @@ def partition_generator(seq_length, window_size, Minimum_Window_Size):
     return partitionSchemes
 
 def readPDBfile(fileName):
+    atom_lst = []
     f = open(fileName, "r")
     lines = f.readlines()
+    k = 0
     for line in lines:
         if line[0:4] == "ATOM" or line[0:6] == "HETATM":
-            print(line.split()[0], line.split()[2], line.split()[3])
-
-
+            atom_lst.append([line.split()[3], line.split()[5], line.split()[2],[line.split()[6], line.split()[7], line.split()[8]],
+                             radius_table[line.split()[2]], math.pi * (radius_table[line.split()[2]]**2)])
+    return atom_lst
 
 def readPDBinfo(fileName):
     f = open(fileName, "r")
@@ -190,4 +214,4 @@ text_file = open("data.txt", "w")
 text_file.write(output)
 text_file.close()
 
-readPDBfile("6cne.pdb")
+print(readPDBfile("1ediA.pdb"))
